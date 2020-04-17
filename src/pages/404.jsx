d1e2c7af81ad808464e7dfade17abbc24432c91e
원더 <wonderbotsupport@naver.com>
2020-04-17 11:05:12 +0900

    Add More More Contents

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,27 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 const NotFound = () => {
+  const [EE, setE] = useState(0)
+  const Up = () => {
+    console.log('g')
+    setE(EE+1)
+  }
   const history = useHistory();
   const back = () => history.goBack();
   return (
     <div className="ui center aligned container">
-      <h1 style={{ fontSize: "10em" }} className="ui inverted header">
-        404
+      <h1 style={{ fontSize: "10em", color: EE >= 3 ? "red" : "white" }} className="ui inverted header" onClick={Up}>
+        {
+          EE >= 3 ? '444' : '404'
+        }
       </h1>
       <h2 style={{ fontSize: "2em" }} className="ui inverted header">
-        이런! 길을 잘못 드셨어요.
+      {
+          EE >= 3 ? '아마 길을 잘못든거겠죠??' : '이런! 길을 잘못 드셨어요.'
+        }
       </h2>
       <a className="ui inverted primary button" href="/">
-        <i className="icon home" />
-        홈으로 돌아가기
+        <i className={EE >= 3 ? "vertically flipped icon home" : "icon home" } />
+        {
+          EE >= 3 ? '집으로 돌아갈 수 있을거 같니' : '홈으로 돌아가기'
+        }
       </a>
       <a className="ui inverted orange button" onClick={back}>
         <i className="icon arrow left" />
-        뒤로 가기
+        {
+          EE >= 3 ? '저주에서 벗어나기' : '앞으로 가기'
+        }
       </a>
       <br />
-      <p className="ui inverted header">이런..이런...이런....이런!!</p>
+      <p className="ui inverted header"> {
+          EE >= 3 ? '뒤 좀 보세요...ㅎㅎㅎ' : '이런...이런....이런....!!'
+        }</p>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,10 +31,10 @@ const Home = props => {
       </div>
       <div className="ui divider"/>
       <div className="ui stackable grid" style={{paddingTop: '15vh'}}>
-        <div className="twelve wide column">      
+        <div className="ten wide column">      
         <img className="ui image" alt="Slot" src="/images/slot.gif" /> 
       </div>
-      <div className="four wide column">
+      <div className="six wide column">
         <h1 className="ui inverted header">중독성 있는 도박.</h1>
         <div className="desc">가상화폐를 이용한 다양한 도박 명령어를 사용할 수 있습니다.<br/>디스코드에서 작은 카지노를 만나보세요.</div>
       </div>
@@ -44,13 +44,12 @@ const Home = props => {
       <br/>
       <div className="ui stackable grid">
         
-      <div className="five wide column" >
-        <br/><br/>
+      <div className="six wide column" >
         <h1 className="ui inverted header">시도때도 없이 변하는 아이템시장.</h1>
         <div className="desc">아이템의 가격은 계속 변경됩니다. <br/>현실의 시세와 비슷하죠. 저렴할 때 구매해서, 가격이 올랐을 때 판매하여 큰 수익을 내보세요!<br/>이 뿐만 아니라 가격 변화 뉴스로 볼 수도 있습니다!<br/>즐거운 디스코드를 만나보세요</div>
       </div>
-      <div className="eleven wide column" style={{}}>      
-        <img className="ui right floated image" alt="Slot" src="/images/item.png" /> 
+      <div className="ten wide column" style={{}}>      
+        <img className="ui right floated image" alt="Item" src="/images/item.png" /> 
       </div>
       </div>
       <br/>
@@ -58,12 +57,25 @@ const Home = props => {
       <br/>
       <div className="ui stackable grid">
         <div className="ten wide column">      
-        <img className="ui image" alt="Slot" src="/images/mask.png" /> 
+        <img className="ui image" alt="delivery" src="/images/delivery.png" /> 
       </div>
       <div className="six wide column">
+        <h1 className="ui inverted header">유용함과 편리함.</h1>
+        <div className="desc">택배 명령어뿐만 아니라 환율 기능과 원소기호 등 유용한 기능을 디스코드에서 만나보세요</div>
+      </div>
+      </div>
+      <br/>
+      <div className="ui divider"/>
+      <br/>
+      <div className="ui stackable grid">
+      <div className="seven wide column">
         <h1 className="ui inverted header">시즌 명령어</h1>
         <div className="desc">코로나때는 마스크와 선별진료소, 올림픽때는 순위 기능 등 시즌에 맞는 다양한 명령어가 계속 업데이트 됩니다!<br/>따로 다른 봇이 필요없이 다양한 기능을 통합하여 서비스하고 있습니다.<br/>다양한 기능을 만나보세요.</div>
       </div>
+        <div className="nine wide column">      
+        <img className="ui image" alt="mask" src="/images/mask.png" /> 
+      </div>
+      
       </div>
     <br/><br/>
     </div>
